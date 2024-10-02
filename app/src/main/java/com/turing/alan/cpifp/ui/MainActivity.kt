328a478e6fcdf63af7f72f3ad67dd7346b142ac9
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-10-02 14:43:39 +0200

    Primer_commit

--- app/src/main/java/com/turing/alan/cpifp/ui/MainActivity.kt
+++ app/src/main/java/com/turing/alan/cpifp/ui/MainActivity.kt
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.turing.alan.cpifp.R
+import com.turing.alan.cpifp.data.InMemoryChampionsRepository
+import com.turing.alan.cpifp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val repository = InMemoryChampionsRepository.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
